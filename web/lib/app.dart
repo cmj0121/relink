0a11d76ad6f33c7f199cc492b001165385517c02
cmj <cmj@cmj.tw>
2024-05-24 13:56:33 +0800

    feat(ui): show the squashed link

--- web/lib/app.dart
+++ web/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ReLinkApp extends StatelessWidget {
   static String title = 'ReLink';
@@ -44,7 +45,8 @@ class SquashLink extends StatefulWidget {
 class _SquashLinkState extends State<SquashLink> {
   final _textController = TextEditingController();
 
-  String? _errorText;
+  bool _error = false;
+  String? _squashedLink;
 
   @override
   void dispose() {
@@ -61,6 +63,10 @@ class _SquashLinkState extends State<SquashLink> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             inputLinkField(),
+            SizedBox(height: 20),
+            Loading(icon: Icons.keyboard_arrow_down_outlined),
+            SizedBox(height: 20),
+            squashLinkField(),
           ],
         ),
       ),
@@ -71,11 +77,47 @@ class _SquashLinkState extends State<SquashLink> {
     return TextField(
       controller: _textController,
       decoration: InputDecoration(
-        labelText: 'Enter a URL',
-        errorText: _errorText,
+        prefixIcon: Icon(Icons.arrow_forward_ios_outlined),
+        hintText: 'Enter a URL',
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: _error ? Colors.red : Colors.grey),
+        ),
       ),
       textInputAction: TextInputAction.go,
       onSubmitted: squashLink,
+    );
+  }
+
+  Widget squashLinkField() {
+    return Opacity(
+      opacity: _squashedLink == null ? 0.0 : 1.0,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.copy),
+              onPressed: _squashedLink == null ? null : copyLink,
+            ),
+            SizedBox(width: 10),
+            Text(
+              _squashedLink ?? '',
+              style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void copyLink() {
+    if (_squashedLink == null) return;
+
+    Clipboard.setData(ClipboardData(text: _squashedLink!));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Copied to clipboard: $_squashedLink'),
+      ),
     );
   }
 
@@ -83,14 +125,85 @@ class _SquashLinkState extends State<SquashLink> {
     final target = Uri.parse(url);
 
     setState(() {
-      _errorText = null;
+        _error = false;
 
       if (target.scheme.isEmpty || target.host.isEmpty) {
-        _errorText = 'You must enter a valid URL';
+        _error = true;
+        _squashedLink = null;
+      } else {
+        _squashedLink = target.host;
       }
     });
   }
 }
 
+class Loading extends StatefulWidget {
+  final int size;
+  final duration;
+  final IconData icon;
+
+  Loading({Key? key, required this.icon, this.size = 5, this.duration = const Duration(seconds: 1)}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: buildAnimation(),
+      ),
+    );
+  }
+
+  Widget buildAnimation() {
+    List<Color> colors = [
+      Colors.grey.shade100,
+      Colors.grey.shade200,
+      Colors.grey.shade300,
+      Colors.grey.shade400,
+      Colors.grey.shade500,
+      Colors.grey.shade600,
+      Colors.grey.shade700,
+      Colors.grey.shade800,
+      Colors.grey.shade900,
+    ];
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        int index = (_controller.value * 10).toInt();
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List<Widget>.generate(widget.size, (i) {
+            return Icon(
+              widget.icon,
+              color: colors[(index - i) % colors.length],
+            );
+          }),
+        );
+      },
+    );
+  }
+}
 
 // vim: set ts=2 sw=2 expandtab:
