37870f32f6ecc6113e023060cad79fa2a2ec9639
cmj <cmj@cmj.tw>
2024-07-10 10:35:05 +0800

    feat(extension): pop-up the message in the active tab

--- tools/extensions/background.js
+++ tools/extensions/background.js
@@ -31,20 +31,56 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
         body: JSON.stringify(payload),
       })
       .then((response) => response.json())
-      .then((text) => copyToClipboard(text, tab));
+      .then((text) => injection(text, tab));
   }
 });
 
-function _copyToClipboard(text) {
-  navigator.clipboard.writeText(text)
-}
-
-function copyToClipboard(text, tab) {
+// using chrome.scripting.executeScript to inject the function
+// into the active tab
+function injection(text, tab) {
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
-    function: _copyToClipboard,
+    function: _injection,
     args: [text],
   });
+}
+
+// inject the css and js into the active tab
+function _injection(text) {
+  // copy the text to the clipboard
+  navigator.clipboard.writeText(text);
+
+  // inject the alert with the customized style
+  let dom = document.createElement("div");
+  dom.textContent = `Copy to clipboard: ${text}`;
+  dom.style.cssText = `
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+
+    padding: 8px;
+
+    border: 1px solid #d3d3d3;
+    border-radius: 5px;
+    font-family: monospace;
+    font-size: 1em;
+    background: #38a846 !important;
+  `;
+  document.body.appendChild(dom);
+
+  // dismiss the alert after 3 seconds
+  setTimeout(() => {
+    // fade out the alert
+    dom.style.transition = "opacity 1s";
+    dom.style.opacity = 0;
+
+    // remove the alert from the DOM after 1 second
+    setTimeout(() => {
+      document.body.removeChild(dom);
+    }, 1000);
+  }, 3000)
 }
 
 // vim: set ts=2 sw=2 et:
